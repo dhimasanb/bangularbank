124671befa821cd74c57b5318e78c4598b05160c
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-03 15:12:39 +0700

    Generate circular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CircularComponent } from './circular/circular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircularComponent
   ],
   imports: [
     BrowserModule
