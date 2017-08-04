7b8fd71df0197833fa8c63af08f8f2c8cff63377
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-04 14:29:31 +0700

    Import http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
@@ -12,7 +13,8 @@ import { SearchComponent } from './search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
