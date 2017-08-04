93a089d87805aadb758c558f36ba006e42712e67
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-08-04 17:20:44 +0700

    Routing, route parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+
+import { appRoutes } from "./app.routes";
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
 import { SearchComponent } from './search/search.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CircularComponent,
-    SearchComponent
+    SearchComponent,
+    UserComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpModule,
     MaterialModule
